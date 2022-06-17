789e0209728ed028beea2a668ab8a73d13642b09
sbshyam <105294247+sbshyam@users.noreply.github.com>
2022-06-17 14:31:21 +0530

    Update alerter.cs

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -22,7 +22,7 @@ namespace AlerterSpace {
                 // let us keep a count of failures to report
                 // However, this code doesn't count failures!
                 // Add a test below to catch this bug. Alter the stub above, if needed.
-                alertFailureCount += 0;
+                alertFailureCount += 1;
             }
         }
         static void Main(string[] args) {
